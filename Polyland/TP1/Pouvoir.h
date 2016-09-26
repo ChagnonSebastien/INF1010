8d3d672eee2899fefc5f146ce8e947cabe4d9e12
PoutineQc <chagnon.s21@gmail.com>
2016-09-26 10:38:02 -0400

    Ajout des descriptions de la classe Pouvoir

--- Polyland/TP1/Pouvoir.h
+++ Polyland/TP1/Pouvoir.h
@@ -13,20 +13,77 @@ Description: les dresseurs sont les etres capables d'attraper des creatures
 
 using namespace std;
 
+/**
+* @brief La classe Pouvoir, pour decrire un pouvoir
+*/
 class Pouvoir
 {
 public:
+
+	// Constructors
+	/**
+	* @brief Constructeur par default
+	*/
 	Pouvoir();
+
+	/**
+	* @brief Constructeur par parametres
+	* @param nom Nom du pouvoir
+	* @param nombreDeDegats Nombre de degats infliges par le pouvoir
+	* @param energieNecessaire Energie necessaire pour lancer le pouvoir
+	*/
 	Pouvoir(string nom, int nombreDeDegats, int energieNecessaire);
+
+	/**
+	* @brief Destructeur de la classe
+	*/
 	~Pouvoir();
 
+
+	// Getters
+	/**
+	* @brief Pour obtenir le nom du pouvoir
+	* @return Le nom du pouvoir
+	*/
 	string obtenirNom() const;
-	void modifierNom(string nom);
+
+	/**
+	* @brief Pour obtenir le nombre de degats infliges par le pouvoir
+	* @return Le nombre de degats infliges par le pouvoir
+	*/
 	int obtenirNombreDeDegat() const;
-	void modifierNombreDeDegat(int nombreDeDegat);
+
+	/**
+	* @brief Pour obtenir la quantite d'energie necessaire pour lancer le pouvoir
+	* @return La quantite d'energie necessaire pour lancer le pouvoir
+	*/
 	int obtenirEnergieNecessaire() const;
+
+
+	// Setters
+	/**
+	* @brief Pour modifier le nom du pouvoir
+	* @param nom Le nouveau nom du pouvoir
+	*/
+	void modifierNom(string nom);
+
+	/**
+	* @brief Pour modifier le nombre de degats infliges par le pouvoir
+	* @param nom Le nouveau nombre de degats infliges par le pouvoir
+	*/
+	void modifierNombreDeDegat(int nombreDeDegat);
+
+	/**
+	* @brief Pour modifier la quantite d'energie necessaire pour lancer le pouvoir
+	* @param nom La quantite d'energie necessaire pour lancer le pouvoir
+	*/
 	void modifierEnergieNecessaire(int energieNecessaire);
 
+
+
+	/**
+	* @brief Affiche les attributs du pouvoir
+	*/
 	void description();
 
 private:
