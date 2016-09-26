ce02a7b450ad1e2befa45aa1af97806135cb1014
PoutineQc <chagnon.s21@gmail.com>
2016-09-26 10:38:40 -0400

    Modification du header du fichier Pouvoir.h

--- Polyland/TP1/Pouvoir.h
+++ Polyland/TP1/Pouvoir.h
@@ -1,9 +1,9 @@
 /*
-Fichier: Dresseur.h
-Auteur(s): Alexandre MAO
-Date de creation: 31 aout 2016
-Date de modification: 4 septembre 2016
-Description: les dresseurs sont les etres capables d'attraper des creatures
+Fichier: Pouvoir.h
+Auteur(s): Sébastien CHAGNON, Audrey LABRIE
+Date de creation: 13 septembre 2016
+Date de modification: 26 septembre 2016
+Description: les pouvoir sont détenus par les créatures
 */
 #ifndef POUVOIR_H
 #define POUVOIR_H
