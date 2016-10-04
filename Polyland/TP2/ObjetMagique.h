a6a0b583222c20dc155a67f985eff4a9113b4ce6
PoutineQc <chagnon.s21@gmail.com>
2016-10-03 21:11:44 -0400

    Ajout des headers a certains fichiers header

--- Polyland/TP2/ObjetMagique.h
+++ Polyland/TP2/ObjetMagique.h
@@ -3,19 +3,66 @@
 
 #include <string>
 
+/**
+* @brief La classe ObjetMagique, pour decrire un objet magique
+*/
 class ObjetMagique
 {
 public:
+
+	// Constructors
+	/**
+	* @brief Constructeur par default
+	*/
 	ObjetMagique();
+
+	/**
+	* @brief Constructeur par parametres
+	* @param nom Nom du pouvoir
+	* @param bonus Le bonus de l'objet magique
+	*/
 	ObjetMagique(const std::string &nom, int bonus);
+
+	/**
+	* @brief Destructeur de la classe
+	*/
 	~ObjetMagique();
 
+
+	// Getters
+	/**
+	* @brief Pour obtenir le nom de l'objet magique
+	* @return Le nom de l'objet magique
+	*/
 	std::string obtenirNom() const;
+
+	/**
+	* @brief Pour obtenir le bonus de l'objet magique
+	* @return Le bonus de l'objet magique
+	*/
 	int obtenirBonus() const;
 
+
+	// Setters
+	/**
+	* @brief Pour modifier le nom de l'objet mgique
+	* @param nom Le nouveau nom de l'objet magique
+	*/
 	void modifierNom(const std::string& nom);
+
+	/**
+	* @brief Pour modifier le bonus de l'objet mgique
+	* @param nom Le nouveau bonus de l'objet magique
+	*/
 	void modifierBonus(int bonus);
 
+
+	/**
+	* @brief Redefinition de l'operateur '<<' avec un objet magique en parametre
+	* @param o L'output stream dans lequel ajouter les informations du dresseur
+	* @param objetMagique L'objet magique a afficher les informations
+	* @return L'output stream recu en parametre
+	*/
 	std::ostream& operator<<(std::ostream& o) const;
 
 private:
--- Polyland/TP2/PolyLand.h
+++ Polyland/TP2/PolyLand.h
@@ -8,37 +8,125 @@
 #define MAX_OBJET_MAGIQUE 100
 #define MAX_POUVOIRS 10
 
+/**
+* @brief La classe Polyland, pour decrire le monde de Polyland
+*/
 class PolyLand
 {
 public:
-	PolyLand(); // A MODIFIER... (si necessaire)
-	~PolyLand(); // A MODIFIER... (si necessaire)
-	
-	bool ajouterDresseur(Dresseur* dresseur); // A MODIFIER... (si necessaire)
-	bool retirerDresseur(const std::string& nom); // A MODIFIER... (si necessaire)
+
+	// Constructors
+	/**
+	* @brief Constructeur par default
+	*/
+	PolyLand();
+
+	/**
+	* @brief Destructeur de la classe
+	*/
+	~PolyLand();
+
+
+	/**
+	* @brief Ajouter un dresseur au monde de Polyland
+	* @param dresseur Un pointeur sur le dresseur a ajouter
+	* @return true si l'ajout a ete un succes, false sinon
+	*/
+	bool ajouterDresseur(Dresseur* dresseur);
+
+	/**
+	* @brief Retirer un dresseur au monde de Polyland
+	* @param nom Le nom du dresseur a retirer
+	* @return true si le retrait a ete un succes, false sinon
+	*/
+	bool retirerDresseur(const std::string& nom);
+
+	/**
+	* @brief Redefinition de l'operateur '+=' avec un dresseur
+	* @param dresseur Un pointeur vers le dresseur a ajouter
+	* @return La reference a la classe PolyLand actuelle
+	*/
 	PolyLand& operator+=(Dresseur* dresseur);
+
+	/**
+	* @brief Redefinition de l'operateur '-=' avec un dresseur
+	* @param dresseur Un pointeur vers le dresseur a retirer
+	* @return La reference a la classe PolyLand actuelle
+	*/
 	PolyLand& operator-=(Dresseur* dresseur);
 
-	bool ajouterCreature(const Creature& creature); // A MODIFIER... (si necessaire)
-	bool retirerCreature(const std::string& nom); // A MODIFIER... (si necessaire)
+
+	/**
+	* @brief Ajouter une creature au monde de Polyland
+	* @param creature Creature a ajouter
+	* @return true si l'ajout a ete un succes, false sinon
+	*/
+	bool ajouterCreature(const Creature& creature);
+
+	/**
+	* @brief Retirer une creature au monde de Polyland
+	* @param nom Une reference du nom de la creature a retirer
+	* @return true si le retrait a ete un succes, false sinon
+	*/
+	bool retirerCreature(const std::string& nom);
+
+	/**
+	* @brief Redefinition de l'operateur '+=' avec une creature
+	* @param creature Une reference a la creature a ajouter
+	* @return La reference a la classe PolyLand actuelle
+	*/
 	PolyLand& operator+=(Creature& creature);
+
+	/**
+	* @brief Redefinition de l'operateur '-=' avec une creature
+	* @param creature Une reference a la creature a retirer
+	* @return La reference a la classe PolyLand actuelle
+	*/
 	PolyLand& operator-=(Creature& creature);
 
-	Dresseur* choisirDresseurAleatoire(); // A MODIFIER... (si necessaire)
-	Creature* choisirCreatureAleatoire(); // A MODIFIER... (si necessaire)
 
-	bool attraperCreature(Dresseur* dresseur, const Creature* creature); // A MODIFIER... (si necessaire)
-	bool relacherCreature(Dresseur* dresseur, const std::string& nomCreature); // A MODIFIER... (si necessaire)
+	/**
+	* @brief Pour obtenir un dresseur aleatoire du monde de Polyland
+	* @return Un pointeur vers un dresseur aleatoire
+	*/
+	Dresseur* choisirDresseurAleatoire();
 
+	/**
+	* @brief Pour obtenir une creature aleatoire du monde de Polyland
+	* @return Un pointeur vers une creature aleatoire
+	*/
+	Creature* choisirCreatureAleatoire();
+
+
+	/**
+	* @brief Methode pour qu'un dresseur attrape une creature
+	* @param dresseur Un pointeur vers le dresseur a qui ajouter la creature
+	* @param Creature Un pointeur vers la creature a ajouter au dresseur
+	* @return true si l'ajout a ete un succes, false sinon
+	*/
+	bool attraperCreature(Dresseur* dresseur, const Creature* creature);
+
+	/**
+	* @brief Methode pour qu'un dresseur relache une creature
+	* @param dresseur Un pointeur vers le dresseur a qui retirer une creature
+	* @param nom Une reference du nom de la creature retirer du dresseur
+	* @return true si le retrait a ete un succes, false sinon
+	*/
+	bool relacherCreature(Dresseur* dresseur, const std::string& nomCreature);
+
+
+	/**
+	* @brief Redefinition de l'operateur '<<' avec une instance de PolyLand en parametre
+	* @param o L'output stream dans lequel ajouter les informations du dresseur
+	* @param polyland Une reference a PolyLand
+	* @return L'output stream recu en parametre
+	*/
 	friend ostream& operator<<(ostream& o, const PolyLand& polyland);
 
 		
-	//________TP2_______
-
 private:
 	vector<Creature*> creatures_;
 	vector<Dresseur*> dresseurs_;
-	//________TP2_______
 
 };
 #endif
